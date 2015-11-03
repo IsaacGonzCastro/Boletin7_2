33da61ae0336dfb21abc06e0ba2aa84d7f9fbb72
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2015-11-03 09:50:33 +0100

    Boletin7_2

---
+++ src/boletin7_2/Boletin7_2.java
@@ -0,0 +1,15 @@
+
+package boletin7_2;
+
+public class Boletin7_2 {
+
+    public static void main(String[] args) {
+   short num1;
+   short num2;
+   Comparar obx = new Comparar();
+   num1 = obx.pedirNumero();
+   num2 = obx.pedirNumero();
+   obx.Comparar(num1, num2);
+    }
+    
+}
